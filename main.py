4abef0ef80dc725c89d6b4c5741fd014235c9aa2
xTaube <66015084+xTaube@users.noreply.github.com>
2021-03-10 20:35:53 +0100

    commit

--- main.py
+++ main.py
@@ -2,14 +2,29 @@ import pygame as pyg
 import setup
 
 
+<<<<<<< Updated upstream
 
 
 
+=======
+>>>>>>> Stashed changes
 #initialize pygame
 
 pyg.init()
 
 # create screen
+<<<<<<< Updated upstream
 screen = pyg.display.set_mode((800,600))
 while True:
     pass
+=======
+screen = pyg.display.set_mode((setup.width, setup.height))
+
+gameOn = True
+while gameOn:
+    for event in pyg.event.get():
+        if event.type == pyg.QUIT:
+            gameOn = False
+
+
+>>>>>>> Stashed changes
--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+<<<<<<< Updated upstream
 import configparser as cp
+=======
+import configparser as cp
+
+configParser = cp.RawConfigParser()
+configFileName = "config.cfg"
+configParser.read(configFileName)
+
+height = int(configParser.get('setup', 'height'))
+width = int(configParser.get('setup', 'width'))
+>>>>>>> Stashed changes
